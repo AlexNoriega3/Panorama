4a5970e8fee6435269a31a47cafaef3266cdb088
Alex <anoriega@moddtech.com>
2020-08-14 18:02:09 -0700

    IOS Viewer

--- lib/src/flutter_panorama_ios.dart
+++ lib/src/flutter_panorama_ios.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_panorama/platform_interface.dart';
 
+import 'flutter_panorama_method_channel.dart';
+
 class IosPanoramaView extends FlutterPanoramaPlatform {
-
   @override
-  Widget build(BuildContext context, Map<String, dynamic> creationParams, PanoramaPlatformCallbacksHandler callbacksHandler) {
-    // TODO: implement build
-    return null;
+  Widget build(BuildContext context, Map<String, dynamic> creationParams,
+      PanoramaPlatformCallbacksHandler callbacksHandler) {
+    return UiKitView(
+      viewType: "plugins.vincent/panorama",
+      creationParams: creationParams,
+      creationParamsCodec: const StandardMessageCodec(),
+      onPlatformViewCreated: (int id) {
+        MethodChannelPanoramaPlatform(id, callbacksHandler);
+      },
+    );
+    ;
   }
-
 }
